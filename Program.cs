38c6bc92257da2fd10d70ec9a043d3614b512543
Geraldo Neto <geraldospn26@gmail.com>
2025-08-28 21:01:43 -0300

    Conclusão do Desafio de POO da DIO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Smartphone xs = new Iphone("123456789", "iPhone Xs", "123456789012345", 64);
+xs.Ligar();
+xs.InstalarAplicativo("Instagram");
+
+Smartphone nokialumia = new Nokia("987654321", "Nokia 7.2", "987654321098765", 128);
+nokialumia.ReceberLigacao();
+nokialumia.InstalarAplicativo("WhatsApp");
